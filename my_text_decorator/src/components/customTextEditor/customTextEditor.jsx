6bcfc3229b326eca9d8f45712523a0d4a8facd15
Нестерова Таисия Григорьевна <tayanesterova91@gmail.com>
2023-08-22 19:59:08 +0700

    added buttons on custom editor

--- my_text_decorator/src/components/customTextEditor/customTextEditor.jsx
+++ my_text_decorator/src/components/customTextEditor/customTextEditor.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
-
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import FormatStrikethroughIcon from '@mui/icons-material/FormatStrikethrough';
+import FormatSizeIcon from '@mui/icons-material/FormatSize';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import FormatIndentIncreaseIcon from '@mui/icons-material/FormatIndentIncrease';
+import FormatIndentDecreaseIcon from '@mui/icons-material/FormatIndentDecrease';
 
 import './customTextEditor.css';
 import TextEditorButton from "./textEditorButton";
@@ -62,8 +69,40 @@ function CustomTextEditor(props){
     return (
         <div className="textEditor">
             <div className="textEditorHeader">
-                <TextEditorButton onClick={onBold} style={{float: 'left'}}>
-                    <FormatBoldIcon sx={{fontSize: '15px'}}/>
+                <TextEditorButton onClick={onBold} >
+                    <FormatBoldIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onItalic} >
+                    <FormatItalicIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onUnderlined} >
+                    <FormatUnderlinedIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onCrossed} >
+                    <FormatStrikethroughIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onHeader} >
+                    <FormatSizeIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onList} >
+                    <FormatListBulletedIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onOrder} >
+                    <FormatListNumberedIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onAddIndent}>
+                    <FormatIndentIncreaseIcon sx={{fontSize: '30px'}}/>
+                </TextEditorButton>
+
+                <TextEditorButton onClick={onDeleteIndent}>
+                    <FormatIndentDecreaseIcon sx={{fontSize: '30px'}}/>
                 </TextEditorButton>
             </div>
             <textarea className="editorField" placeholder="Enter your text here and stylize it">
