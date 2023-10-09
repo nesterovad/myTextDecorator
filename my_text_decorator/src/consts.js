d46f80c73e3c71ee5f810f776e1ee7b4ce52ac87
Нестерова Таисия Григорьевна <tayanesterova91@gmail.com>
2023-10-09 20:01:16 +0700

    added some consts for tags

---
+++ my_text_decorator/src/consts.js
@@ -0,0 +1,19 @@
+const boldOpen = '<strong>';
+const boldClose = '</strong>';
+const italicOpen = '<em>';
+const italicClose = '</em>';
+const underlineOpen = '<u>';
+const underlineClose = '</u>';
+const crossOpen = "<s>";
+const crossClose = '</s>';
+
+export {
+    boldOpen,
+    boldClose,
+    italicOpen,
+    italicClose,
+    underlineOpen,
+    underlineClose,
+    crossOpen,
+    crossClose,
+}
